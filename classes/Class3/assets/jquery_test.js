40eee9e7206c9301af0f693506e470c49d4d5791
LeahKat2 <leah1.malitsky@gmail.com>
2016-12-13 21:57:00 +0200

    float right

---
+++ classes/Class3/assets/jquery_test.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    var captionhidden = true;
+
+//    $("img").on("click", function(){
+//
+//        if (captionHidden){
+//               $(this).next().show();
+//              captionHidden  = false;
+//        }else{
+//
+//            $(this).next().hide()
+//            captionHidden = true;
+//        }
+//
+//        });
+
+    var myImg = $ ("img");
+
+    myImg.on("mouseenter")

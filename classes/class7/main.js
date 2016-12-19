ad94ea408fd616f9bec21486bc523775c387a39f
LeahKat2 <leah1.malitsky@gmail.com>
2016-12-19 10:03:43 +0200

    revised

---
+++ classes/class7/main.js
@@ -0,0 +1,22 @@
+click the button
+the menu will slide in
+from the right to 20 px
+if its off move it on*/
+
+$(function) {}
+var my right="20px"
+var invisible = true
+$ ("menu"). animate(
+    {right: my right}
+    {queue: false, duration: 500,easing:"linear"}
+    );
+if(invisible){
+my right= "-100px";
+invisible= true;
+
+
+
+}
+
+});
+});

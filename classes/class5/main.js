7a56e9b15bb65818abdaaa43c0e36c42f4909483
LeahKat2 <leah1.malitsky@gmail.com>
2017-01-03 13:36:56 +0200

    Homework 5

---
+++ classes/class5/main.js
@@ -0,0 +1,35 @@
+$(funcation(){
+
+    var $myNavItem = $("nav > ul > li");
+    //var $myDropDown = $myNavItem.find("div");
+
+
+    $myNavItem.on("mouseenter", function(){
+     $(this).find("div").animate(
+         {height: "100px"},
+         {queue: false,
+         duration: 1000,
+         easing: "linear",
+         start: function(){
+            $(this).css("border-width", "3px");
+         }
+         }
+         );
+
+
+    }).on("mouseleave", function(){
+     $(this).find("div").animate(
+         {height: "0px"},
+         {queue: false,
+          duration: 500,
+          easing: "linear",
+          complete: function(){
+              $(this).css("border-width", "0");
+          }
+         }
+
+
+     );
+
+   });
+  });

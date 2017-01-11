15da2bcaeebd7f559380471be82a7ebfa3664545
LeahKat2 <leah1.malitsky@gmail.com>
2017-01-11 20:04:52 +0200

    hw

--- assignments/class7/main.js
+++ assignments/class7/main.js
@@ -1,47 +1,14 @@
-/*click the button
-    the menu will slide in from the right -> change right to 20px
-    check if it's on the page, move it off
-    if its off, move it on*/
-
-    $ (function(){
-
-        var invisible = true;
-        var myRight = "20px";
-
-        $("#clickme").on("click", function(){
-            $("menu").animate(
-                {right: myRight},
-                {que:false, duration: 500, easing: "linear"}
-
-            );
-
-            if (invisible){
-                myRight = "-100px";
-                invisible = false;
-
-            }else{
-                myRight = "20px";
-                invisible = true;
-            }
-       });
-
-    $("#myform").submit(function(e){
-       e.preventDefault();
-    });
-
-        $("#color_red").prop("checked", true);
-        $("input[name='myage']").val("18");
-         $("input[type='checkbox']").change(function)){
-        //alert("the value is: " + $(this).val());
-        $(this).next().css("color",$(this).val());
 
 
+$('#saturate').on "input change", ->
+  val = $(this).val()
+  saturate = 'saturate(' + val + '%)'
+  $('#saturateAmount').text(val)
+  $('#circle').css('-webkit-filter', saturate)
 
-
-
-             var s = "50";
-             var l = "50";
-             $("#mycircle").css("background", "hsl(0 "+h+", "+s+", "+l+")")
-              $("#mycircle").date("color","hsl(0, "+s+"%");
-         });
-        });
+  $('#lightness').on "input change", ->
+  val = $(this).val()
+  saturate = 'saturate(' + val + '%)'
+  $('#saturateAmount').text(val)
+  $('#circle').css('-webkit-filter', lightness)
+  )};

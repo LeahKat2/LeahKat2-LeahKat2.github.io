ddbc74ce39d6780bced42b8349308a32aa04515f
LeahKat2 <leah1.malitsky@gmail.com>
2017-01-16 17:08:27 +0200

    Class Asignment Eight

--- assignments/class7/main.js
+++ assignments/class7/main.js
@@ -6,9 +6,3 @@ $('#saturate').on "input change", ->
   $('#saturateAmount').text(val)
   $('#circle').css('-webkit-filter', saturate)
 
-  $('#lightness').on "input change", ->
-  val = $(this).val()
-  saturate = 'saturate(' + val + '%)'
-  $('#saturateAmount').text(val)
-  $('#circle').css('-webkit-filter', lightness)
-  )};

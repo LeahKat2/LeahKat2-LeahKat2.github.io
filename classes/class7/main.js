43c46c6217ad3c3abb02dfddce2b6316cb95b648
LeahKat2 <leah1.malitsky@gmail.com>
2017-01-18 21:12:19 +0200

    assignment seven

--- classes/class7/main.js
+++ classes/class7/main.js
@@ -1,22 +1,59 @@
-click the button
-the menu will slide in
-from the right to 20 px
-if its off move it on*/
+/*click the button
+the menu will slide in from the right -> change right to 20px
+check if its on the page, move it off
+if its off, move it on*/
 
-$(function) {}
-var my right="20px"
-var invisible = true
-$ ("menu"). animate(
-    {right: my right}
-    {queue: false, duration: 500,easing:"linear"}
+$(function){
+
+    var invisible = true;
+    var myRight = "20px";
+
+$("#clickme").on("click", function(){
+    $("#menu").animate(
+    {right: myRight},
+    {queue: false, duration: 500, easing: "linear"}
+
+
     );
-if(invisible){
-my right= "-100px";
-invisible= true;
+
+    if (invisible){
+        myRight= "=1"
+    }
 
 
 
-}
+
+
+$("#color_red)".prop("checked", true)
+$("input"[name='myage']").val("18");
+$("input[type='checkbox']").change(function(){
+    //alert("the value is: ' + $(this).val());
+$(this).next().css("color",$(this).val());
+
+
+
+    //<div id="circle" data-color="hsl(0,50%,50%)">
+
+    /*#circle::after{
+    content: attr(data-color);
+
+    }
+
+     var s = "50";
+     var l = "50";
+     $("mycircle").css("background", "hsl(0, "+s+%, "+l+%)"); //hsl(4, 40%, 50%)
+     $("#mycircle").data("color", "hsl(0, "+s+"%, "+l+"%)");*/
+
+});
+
+var mySlider1 = $("#saturation");
+var mySlider2 = $("#lightness");
+var myCircle = $("#circle");
+
+
+$("#saturation, #lightness").on("change mousemove", function(){
+    myCircle.css("background","hsl(0,"+mySlider1.val()+"%,"+mySlider2.val()+"%");
+    myCircle.css("color","hsl(0,"+mySlider1.val()+"%,"+mySlider2.val()+"%");
 
 });
 });
